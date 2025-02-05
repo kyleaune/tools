7fdbfbddf580cb4f2b692b7796d8e0598158f4c2
kyleaune <aune.kyle@gmail.com>
2025-02-05 09:21:33 -0500

    including full ACS-5

--- ACS_mass_backup.R
+++ ACS_mass_backup.R
@@ -12,6 +12,8 @@ install.packages(setdiff(pkgs, rownames(installed.packages())))
 # Loading all the packages
 lapply(pkgs, library, character.only = TRUE, quietly = TRUE, verbose = FALSE)
 
+setwd("/Volumes/T7 Shield/Census_ACS_Backup/")
+
 # 5 year ACS at census tract level by state (2009-2023)
 yrs <- 2009:2023
 
@@ -48,13 +50,53 @@ foreach(ii = seq_along(s)) %dopar% {
         variables = v$dp05var,
         output = "wide")
       
-      write_parquet(x, paste0("/Volumes/T7 Shield/Census_ACS_Backup/DP05",
-                              "_", s[ii], "_tracts_", y, ".parquet"))
-      write.csv(v, paste0("/Volumes/T7 Shield/Census_ACS_Backup/DP05",
-                          "_", s[ii], "_tracts_", y, "_vars.csv"))
+      write_parquet(x, paste0("DP05", "_", s[ii], "_tracts_", y, ".parquet"))
+      write.csv(v, paste0("DP05", "_", s[ii], "_tracts_", y, "_vars.csv"))
     },
     y = yrs,
     v = vars.dp05,
+    SIMPLIFY = FALSE)
+  },
+  # If an error occurs, print a message, return NA, and keep going
+  error = function(e) {
+    message(paste0("And error occurred on file ", fn.nldas[ii]))
+    print(e)
+    return(NA)
+  },
+  # If a warning occurs, print a message, download the file, and keep going
+  warning = function(w) {
+    message("A warning occurred")
+    print(w)
+  })
+  
+}
+
+stopCluster(cl)
+
+# Downloading all years' ACS5 at census tract level by state
+cl <- makeCluster(ceiling(length(state.abb) / (length(state.abb) %% detectCores() + 1)))
+registerDoParallel(cl)
+
+foreach(ii = seq_along(s)) %dopar% {
+  require(dplyr)
+  require(tidycensus)
+  require(arrow)
+  
+  tryCatch({
+    mapply(function(y, v) {
+      x <- get_acs(
+        geography = "tract",
+        state = s[ii],
+        year = y,
+        survey = "acs5",
+        variables = v$acs5,
+        output = "wide")
+      
+      write_parquet(x, paste0("ACS5", "_", s[ii], "_tracts_", y, ".parquet"))
+      write.csv(v, paste0("ACS5", "_", s[ii], "_tracts_", y, "_vars.csv"))
+    },
+    y = yrs,
+    v = vars.acs5,
     SIMPLIFY = FALSE)
   },
   # If an error occurs, print a message, return NA, and keep going
